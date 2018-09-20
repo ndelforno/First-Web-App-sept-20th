edeb4eed965f8262afb54c73a164e19e08a401ce
Ndelforno <nicolas.delforno@gmail.com>
2018-09-20 17:12:19 -0400

    exercise done

--- server.rb
+++ server.rb
@@ -7,3 +7,22 @@ end
 get '/portfolio' do
   erb :gallery
 end
+
+get '/about_me' do
+  @skills = ["Git", "HTML", "CSS", "Ruby"]
+  @interests =  ['cats', 'art', 'music', 'films', 'coffe' ]
+  erb :about_me
+end
+
+get '/favorites' do
+  @fav_links = ["Books I read", "Games I play", "Music I listen"]
+  erb :favorites
+end
+
+get '/' do
+  redirect to('/home')
+end
+
+get '/gallery' do
+  redirect to('portfolio')
+end
